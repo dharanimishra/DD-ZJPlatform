c5f4ea3dfca9b97d6713a8029f137b29c7e8367a
Prabu Uthirapathi <u.prabu@gmail.com>
2012-09-13 11:22:23 +0530

    Log4j used in this service class

--- ziksana-app/zpolls/src/main/java/com/ziksana/service/polls/impl/PollServiceImpl.java
+++ ziksana-app/zpolls/src/main/java/com/ziksana/service/polls/impl/PollServiceImpl.java
@@ -3,33 +3,60 @@
  */
 package com.ziksana.service.polls.impl;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.ziksana.domain.polls.PollQuestion;
 import com.ziksana.domain.polls.PollQuestionResponse;
+
+import com.ziksana.persistence.polls.PollMapper;
+import com.ziksana.persistence.polls.PollQuestionMapper;
+import com.ziksana.persistence.polls.PollQuestionResponseMapper;
 import com.ziksana.service.polls.PollService;
 
 /**
  * @author prabu
- *
+ * 
  */
 public class PollServiceImpl implements PollService {
 
-	/* (non-Javadoc)
+	private static Logger logger = Logger.getLogger(PollServiceImpl.class);
+	
+	@Autowired
+	public PollMapper pollMapper;
+	@Autowired
+	public PollQuestionMapper pollQuestionMapper;
+	@Autowired
+	public PollQuestionResponseMapper pollQuestionResponseMapper;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.ziksana.service.polls.PollService#getPoll(java.lang.Integer)
 	 */
 	@Override
 	public PollQuestion getPoll(Integer pollId) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("getPoll method is invoked");
+		return pollQuestionMapper.getPoll(pollId);
+
 	}
 
-	/* (non-Javadoc)
-	 * @see com.ziksana.service.polls.PollService#answerPoll(java.lang.Integer, java.lang.Integer)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ziksana.service.polls.PollService#answerPoll(java.lang.Integer,
+	 * java.lang.Integer)
 	 */
 	@Override
 	public PollQuestionResponse answerPoll(Integer pollQuestionId,
 			Integer pollAnswerId) {
+        logger.info("answerPoll method is invoked");
+		// Create pollquestionresponse object based on pollquestionid
+		PollQuestionResponse pollQuestionResponse = new PollQuestionResponse();
+		pollQuestionResponseMapper.updateByPrimaryKey(pollQuestionResponse);
+
 		// TODO Auto-generated method stub
-		return null;
+		return pollQuestionResponse;
 	}
 
 }
