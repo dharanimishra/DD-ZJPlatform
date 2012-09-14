be9fc13743e66ff9ab83132bbf21ab1984d71cdb
Prabu Uthirapathi <u.prabu@gmail.com>
2012-09-14 12:51:28 +0530

    Controller for poll functionality

---
+++ ziksana-app/ziksana-web/src/main/java/com/spring/controllers/PollController.java
@@ -0,0 +1,37 @@
+package com.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ziksana.domain.polls.PollQuestion;
+import com.ziksana.service.polls.PollService;
+
+
+
+
+@Controller
+@RequestMapping(value = "/*")
+public class PollController {
+	
+	@Autowired
+	private PollService pollService;
+	
+	
+	
+	
+	@RequestMapping(value = "/homePage.htm", method = RequestMethod.GET, params = {})
+	public @ResponseBody
+	PollQuestion getPoll(Integer pollId)
+	{
+		return pollService.getPoll(pollId);
+		
+	}
+	
+	
+	
+	
+
+}
