76cb25c5fdf95f35b2c505ce379797b480afe9e2
bhashasp <bhashasp@gmail.com>
2012-09-17 17:38:52 +0530

    File name changed to Learning Program

---
+++ ziksana-app/zcourse/src/main/java/com/ziksana/service/course/LearningProgramService.java
@@ -0,0 +1,18 @@
+package com.ziksana.service.course;
+
+
+public interface LearningProgramService {/*
+	
+	public AcademicProgram getAcademicProgramDetails();
+
+	public List<AcademicProgram> getListOfAcademicPrograms();
+	
+	public AcademicProgramDTO addAcademicProgram();
+
+	public AcademicProgramDTO updateAcademicProgram();
+
+
+	public AcademicProgramDTO removeAcademicProgram();
+
+
+*/}
---
+++ ziksana-app/zcourse/src/main/java/com/ziksana/service/course/impl/LearningProgramServiceImpl.java
@@ -0,0 +1,39 @@
+package com.ziksana.service.course.impl;
+
+import com.ziksana.service.course.LearningProgramService;
+
+
+public class LearningProgramServiceImpl implements LearningProgramService {/*
+
+	@Override
+	public AcademicProgramDTO getAcademicProgramDetails() {
+
+		
+		return null;
+	}
+
+	@Override
+	public List<AcademicProgramDTO> getListOfAcademicPrograms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AcademicProgramDTO addAcademicProgram() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AcademicProgramDTO updateAcademicProgram() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AcademicProgramDTO removeAcademicProgram() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+*/}
