e7a04c802b377c1cdbb2e86de48a3a111252e14f
shalingarg <shalingarg@yahoo.com>
2012-10-17 14:35:03 +0530

    Update ziksana-app/zdomain/src/main/java/com/ziksana/domain/utilz/SubjectClassification.java
    Rehashed SubjectClassification class, does not compile.

--- ziksana-app/zdomain/src/main/java/com/ziksana/domain/utilz/SubjectClassification.java
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/utilz/SubjectClassification.java
@@ -2,128 +2,100 @@ package com.ziksana.domain.utilz;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
 
 import com.ziksana.domain.common.AuditHistory;
-import com.ziksana.id.ZID;
+import com.ziksana.domain.common.Topic;
 
 /**
  * @author bhashasp
  */
-public class SubjectClassification extends AuditHistory{
+public class SubjectClassification extends AuditHistory {
 
-	public SubjectClassification(String subject, String area, String topic,
-			Boolean active) {
-		this.subject = subject;
-		this.area = area;
-		this.topic = topic;
-		this.active = active;
-	}
+	// classification is a Map of Area vs SubjectMap
+	// SubjectMap is a Map of Subject vs TopicList
+	// TopicList is a List of Topics
+	private Map<String, Map<String, List<Topic>>> classification = null;
 
 	public SubjectClassification() {
+		
 	}
 
-	private ZID 		subjClassificationId;
-	private Date 		creationDate		= null;
-	private String 		area				= null;
-	private String 		subject				= null;
-	private String 		topic				= null;
-	private Boolean 	active				= null;
+	public SubjectClassification(String area, String subject, Topic topic) {
+		
+		topic.setArea(area);
+		topic.setSubject(subject);
+		add(topic);
+	}
 	
-	private List<String> areaList			= null;
-	private List<String> subjectList		= null;
-	private List<String> topicList			= null;
-
+	public SubjectClassification(Topic topic) {
+		add(topic);
+	}
 	
-	public List<String> getAreaList() {
-		return areaList;
+	public void add(Topic topic) {
+		if (classification == null) {
+			List<Topic> topicList = new ArrayList<Topic>();
+			Map<String, List<Topic>> subjectMap = new HashMap<String, List<Topic>>();
+			classification = new HashMap<String, Map<String, List<Topic>>>();
+			
+			topicList.add(topic);
+			subjectMap.put(topic.getSubject(), topicList);
+			classification.put(topic.getArea(), subjectMap);
+			
+			return;
+		}
+		
+		Map<String, List<Topic>>  subjectMap = null;
+		List<Topic>               topicList  = null;
+		
+		subjectMap = classification.get(topic.getArea());
+		if (subjectMap == null) {
+			topicList = new ArrayList<Topic>();
+			topicList.add(topic);
+			
+			subjectMap = new HashMap<String, List<Topic>>();
+			subjectMap.put(topic.getSubject(), topicList);
+			
+			classification.put(topic.getArea(), subjectMap);
+			return;
+		}
+
+		topicList = subjectMap.get(topic.getSubject());
+		if (topicList == null) {
+			topicList = new ArrayList<Topic>();
+			topicList.add(topic);
+			
+			subjectMap.put(topic.getSubject(), topicList);
+		}
+		
+		topicList.add(topic);
+		
+		return;
 	}
-	public void setAreaList(List<String> areaList) {
-		this.areaList = areaList;
+	
+	public Set<String> getAreaSet() {
+		if (classification == null) {
+			return null;
+		}
+		return classification.keySet();
 	}
-	public List<String> getSubjectList() {
-		return subjectList;
-	}
-	public void setSubjectList(List<String> subjectList) {
-		this.subjectList = subjectList;
-	}
-	public List<String> getTopicList() {
-		return topicList;
-	}
-	public void setTopicList(List<String> topicList) {
-		this.topicList = topicList;
+	
+	public Set<String> getSubjectSet(String area) {
+		if (classification == null || classification.get(area) == null) {
+			return null;
+		}
+		return classification.get(area).keySet();
 	}
 
-	/**
-	 * @return the subjClassificationId
-	 */
-	public ZID getSubjClassificationId() {
-		return subjClassificationId;
-	}
-	/**
-	 * @param subjClassificationId the subjClassificationId to set
-	 */
-	public void setSubjClassificationId(ZID subjClassificationId) {
-		this.subjClassificationId = subjClassificationId;
-	}
-	/**
-	 * @return the creationDate
-	 */
-	public Date getCreationDate() {
-		return creationDate;
-	}
-	/**
-	 * @param creationDate the creationDate to set
-	 */
-	public void setCreationDate(Date creationDate) {
-		this.creationDate = creationDate;
-	}
-	/**
-	 * @return the subject
-	 */
-	public String getSubject() {
-		return subject;
-	}
-	/**
-	 * @param subject the subject to set
-	 */
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	/**
-	 * @return the area
-	 */
-	public String getArea() {
-		return area;
-	}
-	/**
-	 * @param area the area to set
-	 */
-	public void setArea(String area) {
-		this.area = area;
-	}
-	/**
-	 * @return the topic
-	 */
-	public String getTopic() {
-		return topic;
-	}
-	/**
-	 * @param topic the topic to set
-	 */
-	public void setTopic(String topic) {
-		this.topic = topic;
-	}
-	/**
-	 * @return the active
-	 */
-	public Boolean getActive() {
-		return active;
-	}
-	/**
-	 * @param active the active to set
-	 */
-	public void setActive(Boolean active) {
-		this.active = active;
+	public List<Topic> getTopicList(String area, String subject) {
+		if (classification == null || classification.get(area) == null) {
+			return null;
+		}
+		return classification.get(area).get(subject);
 	}
 
 	@Override
@@ -131,5 +103,4 @@ public class SubjectClassification extends AuditHistory{
 		return "SubjectClassification [subject=" + subject + ", area=" + area
 				+ ", topic=" + topic + ", active=" + active + "]";
 	}
-	
 }
