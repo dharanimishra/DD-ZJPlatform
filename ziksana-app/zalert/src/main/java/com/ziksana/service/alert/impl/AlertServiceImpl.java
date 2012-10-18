be67e3cb17ea2cb9c43a52035c5d1f143f6eced0
Prabu Uthirapathi <u.prabu@gmail.com>
2012-10-18 15:44:20 +0530

    Initial checkin AlertServiceImpl

---
+++ ziksana-app/zalert/src/main/java/com/ziksana/service/alert/impl/AlertServiceImpl.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.ziksana.service.alert.impl;
+
+import java.util.List;
+
+import com.ziksana.domain.alerts.Alert;
+import com.ziksana.service.alert.AlertsService;
+
+/**
+ * @author prabu
+ *
+ */
+public class AlertServiceImpl implements AlertsService {
+
+	/* (non-Javadoc)
+	 * @see com.ziksana.service.alert.AlertsService#getAlertList()
+	 */
+	@Override
+	public List<Alert> getAlertList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ziksana.service.alert.AlertsService#createAlertItem(com.ziksana.domain.alerts.Alert)
+	 */
+	@Override
+	public void createAlertItem(Alert AlertItem) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ziksana.service.alert.AlertsService#editAlertItem(com.ziksana.domain.alerts.Alert)
+	 */
+	@Override
+	public void editAlertItem(Alert AlertItem) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.ziksana.service.alert.AlertsService#deleteAlertItem(java.lang.Integer, java.lang.Integer)
+	 */
+	@Override
+	public void deleteAlertItem(Integer memberId, Integer alertId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
