b58f08de532293c110327d0e5338f52fb040e9d8
Prabu Uthirapathi <u.prabu@gmail.com>
2012-10-22 22:30:12 +0530

    BlogService added.

---
+++ ziksana-app/zblogs/src/main/java/com/ziksana/service/blogs/BlogService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ziksana.service.blogs;
+
+import java.util.List;
+
+import com.ziksana.domain.myblogs.BlogPost;
+
+/**
+ * @author prabu
+ *
+ */
+public interface BlogService {
+	
+	public List<BlogPost>  getBlogs();
+	
+	
+}
---
+++ ziksana-app/zblogs/src/main/java/com/ziksana/service/blogs/impl/BlogServiceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.ziksana.service.blogs.impl;
+
+import java.util.List;
+
+import com.ziksana.domain.myblogs.BlogPost;
+import com.ziksana.service.blogs.BlogService;
+
+/**
+ * @author prabu
+ *
+ */
+public class BlogServiceImpl implements BlogService {
+
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see com.ziksana.service.blogs.BlogService#getBlogs()
+	 */
+	@Override
+	public List<BlogPost> getBlogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+
+}
