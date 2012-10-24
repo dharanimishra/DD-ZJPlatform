3db46114589d6775f7c73efe34dd600cc225ec12
Prabu Uthirapathi <u.prabu@gmail.com>
2012-10-24 19:46:05 +0530

    Have used ThreadLocal instead of hardcoded value.

--- ziksana-app/zblogs/src/main/java/com/ziksana/service/blogs/impl/BlogServiceImpl.java
+++ ziksana-app/zblogs/src/main/java/com/ziksana/service/blogs/impl/BlogServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.ziksana.domain.myblogs.BlogPost;
 import com.ziksana.persistence.blogs.BlogMapper;
+import com.ziksana.security.util.ThreadLocalUtil;
 import com.ziksana.service.blogs.BlogService;
 
 /**
@@ -29,7 +30,8 @@ public class BlogServiceImpl implements BlogService {
 	@Override
 	public List<BlogPost> getBlogs() {
 		// TODO Auto-generated method stub
-		Integer postingMemberRoleId = Integer.valueOf(100);
+		
+		Integer postingMemberRoleId = Integer.valueOf(ThreadLocalUtil.getToken().getMemberPersonaId().getStorageID());
 		return blogMapper.getBlogs(postingMemberRoleId);
 	}
 	
