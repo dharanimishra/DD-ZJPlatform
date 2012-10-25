ce833484ed5f70f479f29cc32c971098196c6f50
Prabu Uthirapathi <u.prabu@gmail.com>
2012-10-25 17:32:21 +0530

    skeleton implementation of know me better service added

---
+++ ziksana-app/zknowmebetter/src/main/java/com/ziksana/service/knowmebetter/impl/PersonalityTestServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ziksana.service.knowmebetter.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ziksana.domain.common.Choice;
+import com.ziksana.domain.common.Question;
+import com.ziksana.service.knowmebetter.PersonalityTestService;
+
+public class PersonalityTestServiceImpl implements PersonalityTestService {
+
+	@Override
+	public List<Question> getUnansweredQuestions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveAnswer(Question question, Choice userChoice) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<Question, Choice> answeredQuestions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
