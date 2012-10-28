39578f996aa836511c1d8a74fe3f26b60f866844
Ratnesh Kumar Singh <ratneshkumar.singh@vtgindia.com>
2012-10-28 16:09:57 +0530

    Todo Service

---
+++ ziksana-app/ztodo/src/main/java/com/ziksana/domain/member/MemberPersona.java
@@ -0,0 +1,21 @@
+package com.ziksana.domain.member;
+
+
+
+/**
+ * @author
+ * 
+ */
+public class MemberPersona {
+
+	private Integer memberRoleId;
+
+	public Integer getMemberRoleId() {
+		return memberRoleId;
+	}
+
+	public void setMemberRoleId(Integer memberRoleId) {
+		this.memberRoleId = memberRoleId;
+	}
+
+}
