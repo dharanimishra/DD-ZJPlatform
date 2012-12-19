f9fbf39ed5f38b201f980d996d970c2732f6df2f
Selvan Kumar <selvan52@gmail.com>
2012-12-19 12:16:42 +0530

    ZAnnouncements Module Integration

---
+++ ziksana-app/zannouncements/src/main/java/com/ziksana/controller/AnnouncementsController.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.ziksana.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ziksana.service.announcements.AnnouncementService;
+
+
+
+/**
+ * @author vtg-p13
+ *
+ */
+@Controller
+@RequestMapping("/secure")
+public class AnnouncementsController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AnnouncementsController.class);
+
+	@Autowired
+	AnnouncementService announcementService;
+	
+	/**
+	 * Retrive announcement to display 
+	 */
+	@RequestMapping(value = "/showannouncementbyid/{memberRoleId}", method = RequestMethod.GET)
+	public @ResponseBody ModelAndView getAnnouncementById(@PathVariable Integer memberRoleId) {
+		logger.info("Entering showMyAlerts(): " + memberRoleId);
+		ModelAndView mav = new ModelAndView("xml/announcements");
+		
+		mav.addObject("announcements", announcementService.getAnnouncement(memberRoleId));
+		
+		logger.info("announcement ID: " + memberRoleId);
+		return mav;
+	}
+}
---
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/announcements/Announcement.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package com.ziksana.domain.announcements;
+
+import java.util.Date;
+
+/**
+ * @author vtg-p13
+ * 
+ */
+public class Announcement {
+
+	private int announcementId;
+	private Date announcementDate;
+	private String message;
+	private int messageVisibility;
+	private Date validUntil;
+	private int memberRoleId;
+	private int institutionId;
+	private int institutionUnitId;
+	private int curriculumCourseId;
+	private int learningProgramId;
+	private int courseId;
+	private int courseLearningComponentId;
+	/**
+	 * @return the announcementId
+	 */
+	public int getAnnouncementId() {
+		return announcementId;
+	}
+	/**
+	 * @param announcementId the announcementId to set
+	 */
+	public void setAnnouncementId(int announcementId) {
+		this.announcementId = announcementId;
+	}
+	/**
+	 * @return the announcementDate
+	 */
+	public Date getAnnouncementDate() {
+		return announcementDate;
+	}
+	/**
+	 * @param announcementDate the announcementDate to set
+	 */
+	public void setAnnouncementDate(Date announcementDate) {
+		this.announcementDate = announcementDate;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the messageVisibility
+	 */
+	public int getMessageVisibility() {
+		return messageVisibility;
+	}
+	/**
+	 * @param messageVisibility the messageVisibility to set
+	 */
+	public void setMessageVisibility(int messageVisibility) {
+		this.messageVisibility = messageVisibility;
+	}
+	/**
+	 * @return the validUntil
+	 */
+	public Date getValidUntil() {
+		return validUntil;
+	}
+	/**
+	 * @param validUntil the validUntil to set
+	 */
+	public void setValidUntil(Date validUntil) {
+		this.validUntil = validUntil;
+	}
+	/**
+	 * @return the memberRoleId
+	 */
+	public int getMemberRoleId() {
+		return memberRoleId;
+	}
+	/**
+	 * @param memberRoleId the memberRoleId to set
+	 */
+	public void setMemberRoleId(int memberRoleId) {
+		this.memberRoleId = memberRoleId;
+	}
+	/**
+	 * @return the institutionId
+	 */
+	public int getInstitutionId() {
+		return institutionId;
+	}
+	/**
+	 * @param institutionId the institutionId to set
+	 */
+	public void setInstitutionId(int institutionId) {
+		this.institutionId = institutionId;
+	}
+	/**
+	 * @return the institutionUnitId
+	 */
+	public int getInstitutionUnitId() {
+		return institutionUnitId;
+	}
+	/**
+	 * @param institutionUnitId the institutionUnitId to set
+	 */
+	public void setInstitutionUnitId(int institutionUnitId) {
+		this.institutionUnitId = institutionUnitId;
+	}
+	/**
+	 * @return the curriculumCourseId
+	 */
+	public int getCurriculumCourseId() {
+		return curriculumCourseId;
+	}
+	/**
+	 * @param curriculumCourseId the curriculumCourseId to set
+	 */
+	public void setCurriculumCourseId(int curriculumCourseId) {
+		this.curriculumCourseId = curriculumCourseId;
+	}
+	/**
+	 * @return the learningProgramId
+	 */
+	public int getLearningProgramId() {
+		return learningProgramId;
+	}
+	/**
+	 * @param learningProgramId the learningProgramId to set
+	 */
+	public void setLearningProgramId(int learningProgramId) {
+		this.learningProgramId = learningProgramId;
+	}
+	/**
+	 * @return the courseId
+	 */
+	public int getCourseId() {
+		return courseId;
+	}
+	/**
+	 * @param courseId the courseId to set
+	 */
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	/**
+	 * @return the courseLearningComponentId
+	 */
+	public int getCourseLearningComponentId() {
+		return courseLearningComponentId;
+	}
+	/**
+	 * @param courseLearningComponentId the courseLearningComponentId to set
+	 */
+	public void setCourseLearningComponentId(int courseLearningComponentId) {
+		this.courseLearningComponentId = courseLearningComponentId;
+	}
+	
+
+	
+}
