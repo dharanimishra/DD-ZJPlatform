bc6e2285e7b9c4c25458800c48c949f0d5b9a4a6
Prabu Uthirapathi <u.prabu@gmail.com>
2013-01-09 13:26:18 +0530

    Duration added to the Reference domain object. service modified.

--- ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/Reference.java
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/Reference.java
@@ -12,6 +12,8 @@ public class Reference {
 	private String title;
 	private String uri;
 	private String kind;
+	private Integer duration;
+	
 	
 	
 	public String getTitle() {
@@ -32,6 +34,14 @@ public class Reference {
 	}
 	public void setKind(String kind) {
 		this.kind = kind;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
 	}
 	
 	
