c306074290b58f16a71931b45fe7870e204e3769
Prabu Uthirapathi <prabu.uthirapathi@vtgindia.com>
2013-01-18 20:29:32 +0530

    educator content and hotspot domain classes added.

---
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/EducatorContent.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.ziksana.domain.course;
+
+/**
+ * @author prabu
+ *
+ */
+public class EducatorContent {
+	
+	Integer contentType;
+	 Integer duration;
+	 String title;
+	 String description;
+	 String coordinates;
+	 String url;
+	 
+	public Integer getContentType() {
+		return contentType;
+	}
+	public void setContentType(Integer contentType) {
+		this.contentType = contentType;
+	}
+	public Integer getDuration() {
+		return duration;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCoordinates() {
+		return coordinates;
+	}
+	public void setCoordinates(String coordinates) {
+		this.coordinates = coordinates;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	 
+	 
+	 
+
+}
