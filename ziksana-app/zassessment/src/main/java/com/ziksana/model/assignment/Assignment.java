27ddf63d9567bb5070789f4e87bbdd4bad9b511a
Prabu Uthirapathi <prabu.uthirapathi@vtgindia.com>
2013-02-10 10:18:26 +0530

    Assignment model and mapper added.

---
+++ ziksana-app/zassessment/src/main/java/com/ziksana/model/assignment/Assignment.java
@@ -0,0 +1,50 @@
+package com.ziksana.model.assignment;
+
+public class Assignment {
+	
+	private Integer id;
+	private String name;
+	private String content;
+	private Integer learningComponentId;
+	private Integer creatingMemberRoleId;
+	
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Integer getLearningComponentId() {
+		return learningComponentId;
+	}
+	public void setLearningComponentId(Integer learningComponentId) {
+		this.learningComponentId = learningComponentId;
+	}
+	public Integer getCreatingMemberRoleId() {
+		return creatingMemberRoleId;
+	}
+	public void setCreatingMemberRoleId(Integer creatingMemberRoleId) {
+		this.creatingMemberRoleId = creatingMemberRoleId;
+	}
+	
+	
+	
+	
+	
+
+}
