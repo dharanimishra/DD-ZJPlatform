badc20c4fd0da03a09a0e33b05444a6ac8ce38d9
Selvan Kumar <selvan52@gmail.com>
2013-03-23 12:52:08 +0530

    Removed unwanted Import

--- ziksana-app/zknowmebetter/src/main/java/com/ziksana/service/knowmebetter/impl/PersonalityTestServiceImpl.java
+++ ziksana-app/zknowmebetter/src/main/java/com/ziksana/service/knowmebetter/impl/PersonalityTestServiceImpl.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.stereotype.Service;
 
-import com.ziksana.constants.ZiksanaConstants;
 import com.ziksana.domain.common.Choice;
 import com.ziksana.domain.common.Question;
 import com.ziksana.domain.common.QuestionResponse;
-import com.ziksana.exception.knowmebetter.KnowmebetterException;
 import com.ziksana.persistence.knowmebetter.PersonalityTestMapper;
 import com.ziksana.security.util.ThreadLocalUtil;
 import com.ziksana.service.knowmebetter.PersonalityTestService;
@@ -40,12 +37,10 @@ public class PersonalityTestServiceImpl implements PersonalityTestService {
 	@Override
 	public void saveAnswer(Question question, Choice userChoice) {
 		Integer memberRoleId = Integer.valueOf(ThreadLocalUtil.getToken().getMemberPersonaId().getStorageID());
-		try{
+		
 		personalityTestMapper.saveAnswer(question, userChoice,memberRoleId);
-		}
-		catch(BadSqlGrammarException ex){
-			throw new KnowmebetterException(ZiksanaConstants.KNOWMEBETTER_SQL_DATACCESS_ERROR, ex.getSQLException());
-		}
+		
+		
 
 	}
 
@@ -65,12 +60,8 @@ public class PersonalityTestServiceImpl implements PersonalityTestService {
 	@Override
 	public void updateAnswer(Question question, Choice userChoice) {
 		
-		try{
+	
 		personalityTestMapper.updateAnswer(question, userChoice);
-		}
-		catch(BadSqlGrammarException ex){
-			throw new KnowmebetterException(ZiksanaConstants.KNOWMEBETTER_SQL_DATACCESS_ERROR, ex.getSQLException());
-		}
 		
 	}
 
@@ -80,15 +71,8 @@ public class PersonalityTestServiceImpl implements PersonalityTestService {
 	public List<Question> getUnansweredQuestionsbyId(Integer questionBankId) {
 		 List<Question> questionList = new ArrayList<Question>();
 			Integer memberRoleId = Integer.valueOf(ThreadLocalUtil.getToken().getMemberPersonaId().getStorageID());
-			try{
+		
 			questionList =personalityTestMapper.getUnansweredQuestionsbyId(memberRoleId, questionBankId);
-			}
-			catch(BadSqlGrammarException ex){
-				throw new KnowmebetterException(ZiksanaConstants.KNOWMEBETTER_SQL_DATACCESS_ERROR, ex.getSQLException());
-			}
-			if(questionList.isEmpty()){
-				throw new KnowmebetterException(ZiksanaConstants.KNOWMEBETTER_NO_QUESTION_FOUND);
-			}
 			
 			return questionList;
 	}
