fb7d5d4b1c9d3818e51b7afed8504084b212da75
ratneshkumar <ratneshkumar.singh@vtgindia.com>
2013-04-06 11:00:04 +0530

    Refactoring the code for Zutil

--- ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/CourseStatus.java
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/CourseStatus.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+import com.ziksana.util.EnumUtil;
+
 
 
 
@@ -29,10 +31,10 @@ public enum CourseStatus {
 	private static Map<String, Integer> mapUtil = new HashMap<String, Integer>();
 	
 	static {
-//		EnumUtil enumUtil = new EnumUtil();
-//		mapUtil = enumUtil.getEnumData(category);
-//		System.out.println("Getting static values :category :mapUtil :"
-//				+ mapUtil.size());
+		EnumUtil enumUtil = new EnumUtil();
+		mapUtil = enumUtil.getEnumData(category);
+		System.out.println("Getting static values :category :mapUtil :"
+				+ mapUtil.size());
 	}
 
 	private CourseStatus(int id, String name) {
