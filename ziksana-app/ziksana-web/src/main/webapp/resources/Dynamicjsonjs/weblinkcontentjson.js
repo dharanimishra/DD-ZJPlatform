a4942e6846b522f8bc7b0dec15143a3381cb7f7b
ratneshkumar <ratneshkumar.singh@vtgindia.com>
2013-05-21 15:44:49 +0530

    Subject Area drop down menu script added

--- ziksana-app/ziksana-web/src/main/webapp/resources/Dynamicjsonjs/weblinkcontentjson.js
+++ ziksana-app/ziksana-web/src/main/webapp/resources/Dynamicjsonjs/weblinkcontentjson.js
@@ -1,5 +1,141 @@
 // JavaScript Document
 
+$(document)
+		.ready(
+				function(e) {
+					$.get('/ziksana-web/zcourse/getsubjectarea', {}, function(
+							data) {
+						options = data;
+						var option_string = '';
+						for (i in options) {
+							label = options[i].label;
+							value = options[i].value;
+							if (i == 0) {
+								option = '<option selected="selected" value="'
+										+ value + '">' + label + '</option>';
+							} else
+								option = '<option value="' + value + '">'
+										+ label + '</option>';
+
+							option_string += option;
+						}
+						$('#Careaddl').html(option_string);
+
+					});
+
+					// function getSubject() {
+					$("#Careaddl")
+							.change(
+									function(e) {
+										token = '';
+										request_type = 'GET';
+										uri = '/ziksana-web/zcourse/getsubject';
+
+										var Course_Area = '';
+										Course_Area = $('#Careaddl').val();
+										$
+												.get(
+														uri,
+														{
+															'Course_Area' : Course_Area
+														},
+														function(data) {
+															options = data;
+															var option_string = '';
+															var area_string = '<option selected="selected" value='
+																	+ Course_Area
+																	+ '>'
+																	+ Course_Area
+																	+ '</option>';
+															option_string += '<option selected="selected" value="Select Subject">Select Subject</option>';
+															for (i in options) {
+																label = options[i].label;
+																value = options[i].value;
+																if (i == 0) {
+																	option = '<option  value="'
+																			+ value
+																			+ '">'
+																			+ label
+																			+ '</option>';
+																} else
+
+																	option = '<option value="'
+																			+ value
+																			+ '">'
+																			+ label
+																			+ '</option>';
+
+																option_string += option;
+															}
+
+															$('#Careaddl')
+																	.html(
+																			area_string);
+															$('#Csubjectddl')
+																	.html(
+																			option_string);
+
+														});
+										var topic = '<option value="Select Topic">Select Topic</option>';
+										$('#Ctopicddl').html(topic);
+									});
+
+					// function getTopic() {
+
+					$("#Csubjectddl")
+							.change(
+									function(e) {
+										console
+												.log("Inside subject change handler");
+										uri = '/ziksana-web/zcourse/gettopic';
+										token = '';
+										request_type = 'GET';
+										var Course_Subject = $('#Csubjectddl')
+												.val();
+										var parameters = {
+											"Course_Subject" : Course_Subject
+										};
+
+										$
+												.get(
+														uri,
+														{
+															'Course_Subject' : Course_Subject
+														},
+														function(data) {
+															options = data;
+															var option_string = '';
+															option_string += '<option selected="selected" value="Select Topic">Select Topic</option>';
+															for (i in options) {
+																label = options[i].label;
+																value = options[i].value;
+																if (i == 0) {
+																	option = '<option value="'
+																			+ value
+																			+ '">'
+																			+ label
+																			+ '</option>';
+																} else
+
+																	option = '<option value="'
+																			+ value
+																			+ '">'
+																			+ label
+																			+ '</option>';
+
+																option_string += option;
+															}
+
+															$('#Ctopicddl')
+																	.html(
+																			option_string);
+
+														});
+
+									});
+
+				});
+
 function webLinkContent() {
 
 	// Step 1: Assign Parameters required by the sendMessage function.
