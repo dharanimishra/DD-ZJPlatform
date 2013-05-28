6d3fa2aca70d5d7c15b2ea6a8c29a4c4b9bb4233
ArvindRathodVTG <arrvind.rathod@vtgindia.com>
2013-05-28 09:35:14 +0530

    Added new method to fetch only contents which are not associated with the given component id. The querey is not tested though will require enhancement.

--- ziksana-app/zcourse/src/main/java/com/ziksana/persistence/course/ContentMapper.java
+++ ziksana-app/zcourse/src/main/java/com/ziksana/persistence/course/ContentMapper.java
@@ -36,5 +36,31 @@ public interface ContentMapper {
 	})
 	List<LearningContent> getUserContent(Integer memberId);
 
+	
+	//TODO following is not complete
+	@Select({ "select content.ID, content.ContentType, content.ContentPath, content.ContentName, content.ContentDescription, content.ThumbnailPicturePath " + 
+	" from corlearningcontent content, corlearningcomponentcontent componentcontent where "+
+	" content.AuthoringMemberRoleId=#{memberId,jdbcType=INTEGER} "+
+	" and content.isdelete=false and content.active=true "+
+	" and componentcontent.LearningComponentId != #{componentId,jdbcType=INTEGER} and componentcontent.isdelete=false " })
+	@Results(value = {
+			@Result(property = "learningContentId", column = "ID"),
+			@Result(property = "contentTypeId", column = "ContentType"),
+			@Result(property = "activeFlag", column = "activeFlag"),
+			@Result(property = "active", column = "active"),
+			@Result(property = "contentUrl", column = "ContentPath"),
+			@Result(property = "contentName", column = "ContentName"),
+			@Result(property = "contentDescription", column = "ContentDescription"),
+			@Result(property = "thumbnailPicturePath", column = "ThumbnailPicturePath"),
+			@Result(property = "creationDate", column = "creationDate")
+
+	})
+	List<LearningContent> getContentsToBeAssociated(Integer memberId, Integer componentId);
+	//TODO This method will be required when we will start filtering on the basis of component id, NOT TESTED
+	/**
+	 * This method only returns the list of contents which are not associated for the provided component id 
+	 * @param learningContentId
+	 * @return
+	 */
 	public LearningContent getLearningContent(Integer learningContentId);
 }
