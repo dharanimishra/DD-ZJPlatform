927f2f745f08978ebd4336f683d43689f4d55bea
ratneshkumar <ratneshkumar.singh@vtgindia.com>
2013-05-29 09:42:16 +0530

    ContentStatus file updated

--- ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/ContentType.java
+++ ziksana-app/zdomain/src/main/java/com/ziksana/domain/course/ContentType.java
@@ -9,41 +9,39 @@ import org.slf4j.LoggerFactory;
 import com.ziksana.domain.utils.UTLLookup;
 import com.ziksana.util.UTLLookUpUtil;
 
+public enum ContentType {
 
-public enum ContentType  {
+	VIDEO(), ENHANCED_VIDEO(), AUDIO(), TEXTUAL(), PDF(), DOC(), PPT(), EXCEL(), IMAGE(), LINK();
 
-	
-	VIDEO(),ENHANCED_VIDEO(),AUDIO(),TEXTUAL(), PDF(),DOC(),PPT(),EXCEL(),IMAGE(),LINK();
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(ContentType.class);
-	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ContentType.class);
+
 	private int id;
-	
+
 	private String name;
 
 	private final static String CATEGORY = "corLearningContent_Content Type";
-	
-	
-	private ContentType(){
+
+	private ContentType() {
 	}
 
 	private static boolean initialized = false;
-    
-    
-	
-	private static synchronized void initialize(){
-        if (initialized) { return; }
+
+	private static synchronized void initialize() {
+		if (initialized) {
+			return;
+		}
 		ContentType[] contentTypes = ContentType.values();
 		for (ContentType contentType : contentTypes) {
-			//System.out.println("contentType " + contentType.name());
-			UTLLookup utlLookup = UTLLookUpUtil.getUTLLookUp(CATEGORY, contentType.name());
-			contentType.id=utlLookup.getLookupValueId();
-			contentType.name=utlLookup.getLookupValue();
+			UTLLookup utlLookup = UTLLookUpUtil.getUTLLookUp(CATEGORY,
+					contentType.name());
+			contentType.id = utlLookup.getLookupValueId();
+			contentType.name = utlLookup.getLookupValue();
 		}
-        initialized = true;
+		initialized = true;
 		LOGGER.debug("Content Type initialized " + contentTypes);
 	}
-	
+
 	private ContentType(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -52,7 +50,7 @@ public enum ContentType  {
 	public int getID() {
 		return id;
 	}
-	
+
 	public void setID(Integer id) {
 		this.id = id;
 	}
@@ -60,17 +58,20 @@ public enum ContentType  {
 	public String getName() {
 		return name;
 	}
-	 public static ContentType getContentType(int ID) {
-         if (!initialized) { initialize(); }
-         	for (ContentType t : ContentType.values()) {
-         		if (t.getID() == ID) {
-         			return t;
-         		}
-         	}
 
-         throw new IndexOutOfBoundsException("ContentType ID [" + ID + "] not found");
-	 }
+	public static ContentType getContentType(int ID) {
+		if (!initialized) {
+			initialize();
+		}
+		for (ContentType t : ContentType.values()) {
+			if (t.getID() == ID) {
+				return t;
+			}
+		}
 
+		throw new IndexOutOfBoundsException("ContentType ID [" + ID
+				+ "] not found");
+	}
 
 	public String toString() {
 		return "Content Type [" + getName() + "] ID [" + getID() + "]";
