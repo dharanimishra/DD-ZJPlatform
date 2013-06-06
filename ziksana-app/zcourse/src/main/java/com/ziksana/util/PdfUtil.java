2d1837d56852c609467b6cbef3d5ff57313f748c
Mukut Biharee <mukutbiharee@myziksana.com>
2013-06-06 17:30:32 +0530

    Util added for processing playbook generation

---
+++ ziksana-app/zcourse/src/main/java/com/ziksana/util/PdfUtil.java
@@ -0,0 +1,41 @@
+package com.ziksana.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+public class PdfUtil {
+	public static void main(String[] args){
+		PdfUtil pdfUtil = new PdfUtil();
+		pdfUtil.generatePlayBook("12312","ibnlive...");
+		
+	}
+	public void generatePlayBook(String idStr, String outputfile){
+/*	ProcessBuilder pb = new ProcessBuilder("wkhtmltopdf", "sourcURL", "outputfile");
+	Map<String, String> env = pb.environment();
+	env.put("sourcURL", "http://ibnlive.com");
+	env.put("outputfile", "ibn.pdf");
+	pb.directory(new File("D:/dev"));*/
+	 try {
+		 Runtime rt = Runtime.getRuntime();
+		 Process p = rt.exec("wkhtmltopdf " +idStr+" " + outputfile);
+		//Process p = pb.start();
+		processLog(p.getErrorStream());
+		processLog(p.getErrorStream());
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+	
+	private void processLog(InputStream ins) throws IOException{
+		BufferedReader bff = new BufferedReader(new InputStreamReader(ins));
+		String s = null;
+		while( (s=bff.readLine())!=null){
+			System.out.println(s);
+		}
+	}
+}
